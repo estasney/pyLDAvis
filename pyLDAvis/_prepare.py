bb8000d9b3b8fa56d89ed43081d76fa4400c7437
estasney <estasney@users.noreply.github.com>
2019-09-25 22:19:51 -0400

    mimic columns

--- pyLDAvis/_prepare.py
+++ pyLDAvis/_prepare.py
@@ -294,7 +294,7 @@ def _topic_info(topic_term_dists, topic_proportion, term_frequency, term_topic_f
                           for ls in _job_chunks(lambda_seq, n_jobs)))
     # Create an index to mimic pd.concat([series])
     mimic_index = np.vstack([np.arange(0, R).reshape(-1, 1) for i in np.arange(top_terms.shape[0]//R)]).flatten()
-    top_terms = pd.DataFrame(top_terms, index=mimic_index)
+    top_terms = pd.DataFrame(top_terms, index=mimic_index, columns=np.arange(0, topic_proportion.shape[0]))
     topic_dfs = map(topic_top_term_df, enumerate(top_terms.T.iterrows(), 1))
     return pd.concat([default_term_info] + list(topic_dfs), sort=True)
 
